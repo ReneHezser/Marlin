2760572e647eaae12c3203a7eb7e77c4259e4422
René Hézser <rene@hezser.de>
2018-03-14 22:22:42 +0100

    fixes

--- Marlin/src/config/examples/Mkc/Mk2/René/Configuration.h
+++ Marlin/src/config/examples/Mkc/Mk2/René/Configuration.h
@@ -571,7 +571,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 160, 94,5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 160, 94.5 }
 
 // RH
 /**
