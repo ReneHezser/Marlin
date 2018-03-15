eec573cecc93d3bfa65b1639cb5b62ed7af72ca0
René Hézser <rene@hezser.de>
2018-03-15 19:12:34 +0100

    Fixed Boardname

--- Marlin/src/config/examples/Mkc/Mk2/René/Configuration.h
+++ Marlin/src/config/examples/Mkc/Mk2/René/Configuration.h
@@ -134,7 +134,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD RAMPS4DUE_EFB
+  #define MOTHERBOARD BOARD_RAMPS4DUE_EFB
 #endif
 
 // RH
